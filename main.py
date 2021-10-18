39e2af41ca49701825e15e3ee8e8bde88a2bb908
Seligmann <zachary.a.seligman@gmail.com>
2021-10-18 01:49:02 -0500

    parsing of xml files and error exceptions added

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ using the cv.CascadeClassifier.load method. Afterwards, the detection is done us
 cv.CascadeClassifier.detectMultiScale method, which returns boundary rectangles for the detected
 faces or eyes.
 '''
+
+# FIXME: both positive and negative image directories still need to be added for this code to run properly
+
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
@@ -25,3 +28,34 @@ def detectAndDisplay(frame):
             radius = int(round((w2 + h2)*0.25))
             frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
     cv.imshow('Capture - Face detection', frame)
+    
+parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+args = parser.parse_args()
+face_cascade_name = args.face_cascade
+eyes_cascade_name = args.eyes_cascade
+face_cascade = cv.CascadeClassifier()
+eyes_cascade = cv.CascadeClassifier()
+#-- 1. Load the cascades
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
+if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
+    print('--(!)Error loading eyes cascade')
+    exit(0)
+camera_device = args.camera
+#-- 2. Read the video stream
+cap = cv.VideoCapture(camera_device)
+if not cap.isOpened:
+    print('--(!)Error opening video capture')
+    exit(0)
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        print('--(!) No captured frame -- Break!')
+        break
+    detectAndDisplay(frame)
+    if cv.waitKey(10) == 27:
+        break
