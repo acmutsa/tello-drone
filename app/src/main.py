c8918211114663e0be6e7b07d4d1aa1561c0f1c8
Seligmann <zachary.a.seligman@gmail.com>
2021-10-29 18:53:21 -0500

    styling changes

--- app/src/main.py
+++ app/src/main.py
@@ -4,41 +4,50 @@ import argparse
 import os
 
 '''
-The following code example will use pretrained Haar cascade models to detect faces and eyes
-in an image. First, a cv.CascadeClassifier is created and the necessary XML file is loaded
-using the cv.CascadeClassifier.load method. Afterwards, the detection is done using the
-cv.CascadeClassifier.detectMultiScale method, which returns boundary rectangles for the detected
-faces or eyes.
+The following code example will use pretrained Haar cascade models to detect
+faces and eyes in an image. First, a cv.CascadeClassifier is created and the
+necessary XML file is loaded using the cv.CascadeClassifier.load method.
+Afterwards, the detection is done using the
+cv.CascadeClassifier.detectMultiScale method, which returns boundary rectangles
+for the detected faces or eyes.
 '''
+
 
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
 
-    #-- Detect faces
+    # Detect faces
     bodies = full_body_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in bodies:
+    for (x, y, w, h) in bodies:
         center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
+        frame = cv.ellipse(frame,
+                           center,
+                           (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = frame_gray[y:y+h, x:x+w]
 
     cv.imshow('Capture - Face detection', frame)
 
-parser = argparse.ArgumentParser(description='Argument parser for tello-drone.')
-parser.add_argument('--body_cascade', help='Path to body cascade.', default='/usr/local/lib/python3.8/site-packages/cv2/data/haarcascade_fullbody.xml')
-# parser.add_argument('--device', help='Path camera.', type=str, default='/dev/video0')
+
+parser = argparse.ArgumentParser(
+    description='Argument parser for tello-drone.'
+)
+parser.add_argument('--body_cascade',
+                    help='Path to body cascade.',
+                    default='/usr/local/lib/python3.8/site-packages/cv2/data/ \
+                    haarcascade_fullbody.xml')
 parser.add_argument('--device', help='Path camera.', default='/dev/video0')
 args = parser.parse_args()
 face_cascade_name = args.body_cascade
 camera = args.device
-full_body_cascade= cv.CascadeClassifier()
+full_body_cascade = cv.CascadeClassifier()
 
-#-- 1. Load the cascades
+# Load the cascades
 if not full_body_cascade.load(cv.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
     exit(0)
 
-#-- 2. Read the video stream
+# Read the video stream
 camera_device = cv.VideoCapture(camera)
 
 if not camera_device.isOpened:
