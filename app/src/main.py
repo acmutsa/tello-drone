f6a1f2f9e80035403b0afe4e488b3129593aec2a
Seligmann <zachary.a.seligman@gmail.com>
2021-11-06 00:31:57 -0500

    main.py was using local data instead of .xml in project. misc changes otherwise.

--- app/src/main.py
+++ app/src/main.py
@@ -3,21 +3,10 @@ import cv2 as cv
 import argparse
 import os
 
-'''
-The following code example will use pretrained Haar cascade models to detect
-faces and eyes in an image. First, a cv.CascadeClassifier is created and the
-necessary XML file is loaded using the cv.CascadeClassifier.load method.
-Afterwards, the detection is done using the
-cv.CascadeClassifier.detectMultiScale method, which returns boundary rectangles
-for the detected faces or eyes.
-'''
-
-
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
 
-    # Detect faces
     bodies = full_body_cascade.detectMultiScale(frame_gray)
     for (x, y, w, h) in bodies:
         center = (x + w//2, y + h//2)
@@ -26,25 +15,20 @@ def detectAndDisplay(frame):
                            (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         faceROI = frame_gray[y:y+h, x:x+w]
 
-    cv.imshow('Capture - Face detection', frame)
+    cv.imshow('Capture - Body detection', frame)
 
 
-parser = argparse.ArgumentParser(
-    description='Argument parser for tello-drone.'
-)
-parser.add_argument('--body_cascade',
-                    help='Path to body cascade.',
-                    default='/usr/local/lib/python3.8/site-packages/cv2/data/ \
-                    haarcascade_fullbody.xml')
+parser = argparse.ArgumentParser(description='Argument parser for tello-drone.')
+parser.add_argument('--body_cascade', help='Path to body cascade.', default='haarcascade_fullbody.xml')
 parser.add_argument('--device', help='Path camera.', default='/dev/video0')
 args = parser.parse_args()
-face_cascade_name = args.body_cascade
+body_cascade_name = args.body_cascade
 camera = args.device
 full_body_cascade = cv.CascadeClassifier()
 
 # Load the cascades
-if not full_body_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
+if not full_body_cascade.load(cv.samples.findFile(body_cascade_name)):
+    print('--(!)Error loading body cascade')
     exit(0)
 
 # Read the video stream
@@ -65,4 +49,3 @@ while True:
     if cv.waitKey(10) == 27:
         break
 
-print(os.system("v4l2-ctl --list-devices"))
