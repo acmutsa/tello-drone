ac0d85be05c5372fc5b979c06fbae04e84492197
Seligmann <zachary.a.seligman@gmail.com>
2021-11-09 13:05:06 -0600

    added x11-apps to run script, changed formatting of container run up to modern standard

--- Dockerfile
+++ Dockerfile
@@ -30,9 +30,11 @@ RUN pip3 install -r requirements.txt
 RUN apt-get --assume-yes install software-properties-common
 RUN apt-get --assume-yes install v4l-utils
 RUN apt-get -y install xauth
+RUN apt-get install -qqy x11-apps
 USER ${user}
 
 ENV DISPLAY $DISPLAY
+
 
 # copy the content of local src directory to the working directory
 COPY app/src/ .
--- tello-drone.sh
+++ tello-drone.sh
@@ -3,10 +3,11 @@
 docker build --build-arg user=$USER --build-arg uid=$(id -u) --build-arg gid=$(id -g) -t myimage:latest -f Dockerfile .
 echo "---------- BUILD FINISHED ----------"
 
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
-
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge - 
 echo "---------- XAUTH FINISHED ----------"
 
-docker run --privileged -ti -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -e XAUTHORITY=/tmp/.docker.xauth myimage:latest
-echo "---------- RUNNING FINISHED----------"
+# docker run --privileged -ti -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw -e XAUTHORITY=/tmp/.docker.xauth myimage:latest
+
+docker run --privileged -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth -e XAUTHORITY=/tmp/.docker.xauth -e DISPLAY=$DISPLAY myimage:latest
+echo "----------XAUTH AND RUNNING FINISHED----------"
 
