577a779030837e3d282f037a0a9cf8f9492ac046
Seligmann <zachary.a.seligman@gmail.com>
2021-11-30 18:29:23 -0600

    live object tracking added (no follow or retreating)

--- app/src/obj_tracking.py
+++ app/src/obj_tracking.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # Image dimensions
-width = 640  
+width = 640
 height = 480
 deadZone = 100
 
@@ -34,46 +34,49 @@ frameHeight = height
 
 global imgContour
 global dir;
+
+
 def empty(a):
     pass
 
-# Define trackbars (Hue, Saturation, threshholds, area)
+
+# Define trackbars (Hue, Saturation, thresholds, area)
 cv2.namedWindow("HSV")
-cv2.resizeWindow("HSV",640,240)
-cv2.createTrackbar("HUE Min","HSV",20,179,empty)
-cv2.createTrackbar("HUE Max","HSV",40,179,empty)
-cv2.createTrackbar("SAT Min","HSV",148,255,empty)
-cv2.createTrackbar("SAT Max","HSV",255,255,empty)
-cv2.createTrackbar("VALUE Min","HSV",89,255,empty)
-cv2.createTrackbar("VALUE Max","HSV",255,255,empty)
+cv2.resizeWindow("HSV", 640, 240)
+cv2.createTrackbar("HUE Min", "HSV", 20, 179, empty)
+cv2.createTrackbar("HUE Max", "HSV", 40, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 148, 255, empty)
+cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)
+cv2.createTrackbar("VALUE Min", "HSV", 89, 255, empty)
+cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
 
 cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters",640,240)
-cv2.createTrackbar("Threshold1","Parameters",166,255,empty)
-cv2.createTrackbar("Threshold2","Parameters",171,255,empty)
-cv2.createTrackbar("Area","Parameters",1750,30000,empty)
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Threshold1", "Parameters", 166, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 171, 255, empty)
+cv2.createTrackbar("Area", "Parameters", 1750, 30000, empty)
 
 
 # Display the true, masked, and edge image within one frame
-def stackImages(scale,imgArray):
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], 
-                        imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], 
-                        cv2.COLOR_GRAY2BGR)
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1],
+                                                                 imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y],
+                                                                                 cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
         ver = np.vstack(hor)
@@ -82,69 +85,79 @@ def stackImages(scale,imgArray):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), 
-                        None,scale, scale)
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]),
+                                         None, scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
 
-# Find contours of object detected
-def getContours(img,imgContour):
+
+# Find contours of object detected given an img (which has Canny edge detection applied) and imgContour (original image)
+def getContours(img, imgContour):
     global dir
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # Store all boundary points in contour, as the curvature of the boundary is unknown
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2:]
+
+    # For all contours with contour area greater than the trackbar area, draw all contours,
     for cnt in contours:
         area = cv2.contourArea(cnt)
         areaMin = cv2.getTrackbarPos("Area", "Parameters")
         if area > areaMin:
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            x , y , w, h = cv2.boundingRect(approx)
+            perimeter = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+            x, y, w, h = cv2.boundingRect(approx)
             cx = int(x + (w / 2))  # center x of the object
             cy = int(y + (h / 2))  # center y of the object
 
             # Find location of object, move accordingly
-            if (cx <int(frameWidth/2)-deadZone):
-                cv2.putText(imgContour, " GO LEFT " , (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
-                cv2.rectangle(imgContour,(0,int(frameHeight/2-deadZone)),(int(frameWidth/2)-deadZone,
-                    int(frameHeight/2)+deadZone),(0,0,255),cv2.FILLED)
+            if (cx < int(frameWidth / 2) - deadZone):
+                cv2.putText(imgContour, " GO LEFT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                cv2.rectangle(imgContour, (0, int(frameHeight / 2 - deadZone)), (int(frameWidth / 2) - deadZone,
+                                                                                 int(frameHeight / 2) + deadZone),
+                              (0, 0, 255), cv2.FILLED)
                 dir = 1
             elif (cx > int(frameWidth / 2) + deadZone):
-                cv2.putText(imgContour, " GO RIGHT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
-                cv2.rectangle(imgContour,(int(frameWidth/2+deadZone),int(frameHeight/2-deadZone)),
-                        (frameWidth,int(frameHeight/2)+deadZone),(0,0,255),cv2.FILLED)
+                cv2.putText(imgContour, " GO RIGHT ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                cv2.rectangle(imgContour, (int(frameWidth / 2 + deadZone), int(frameHeight / 2 - deadZone)),
+                              (frameWidth, int(frameHeight / 2) + deadZone), (0, 0, 255), cv2.FILLED)
                 dir = 2
             elif (cy < int(frameHeight / 2) - deadZone):
-                cv2.putText(imgContour, " GO UP ", (20, 50), cv2.FONT_HERSHEY_COMPLEX,1,(0, 0, 255), 3)
-                cv2.rectangle(imgContour,(int(frameWidth/2-deadZone),0),(int(frameWidth/2+deadZone),
-                    int(frameHeight/2)-deadZone),(0,0,255),cv2.FILLED)
+                cv2.putText(imgContour, " GO UP ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                cv2.rectangle(imgContour, (int(frameWidth / 2 - deadZone), 0), (int(frameWidth / 2 + deadZone),
+                                                                                int(frameHeight / 2) - deadZone),
+                              (0, 0, 255), cv2.FILLED)
                 dir = 3
             elif (cy > int(frameHeight / 2) + deadZone):
-                cv2.putText(imgContour, " GO DOWN ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1,(0, 0, 255), 3)
-                cv2.rectangle(imgContour,(int(frameWidth/2-deadZone),int(frameHeight/2)+deadZone),
-                        (int(frameWidth/2+deadZone),frameHeight),(0,0,255),cv2.FILLED)
+                cv2.putText(imgContour, " GO DOWN ", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                cv2.rectangle(imgContour, (int(frameWidth / 2 - deadZone), int(frameHeight / 2) + deadZone),
+                              (int(frameWidth / 2 + deadZone), frameHeight), (0, 0, 255), cv2.FILLED)
                 dir = 4
-            else: dir=0
+            else:
+                dir = 0
 
-            cv2.line(imgContour, (int(frameWidth/2),int(frameHeight/2)), (cx,cy),(0, 0, 255), 3)
+            cv2.line(imgContour, (int(frameWidth / 2), int(frameHeight / 2)), (cx, cy), (0, 0, 255), 3)
             cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
-            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20), 
-                    cv2.FONT_HERSHEY_COMPLEX, .7,(0, 255, 0), 2)
-            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), 
-                    cv2.FONT_HERSHEY_COMPLEX, 0.7,(0, 255, 0), 2)
-            cv2.putText(imgContour, " " + str(int(x)) + " " + str(int(y)), (x - 20, y - 45), 
-                    cv2.FONT_HERSHEY_COMPLEX,0.7,(0, 255, 0), 2)
-        else: dir=0
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + w + 20, y + 20),
+                        cv2.FONT_HERSHEY_COMPLEX, .7, (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+            cv2.putText(imgContour, " " + str(int(x)) + " " + str(int(y)), (x - 20, y - 45),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+        else:
+            dir = 0
+
 
 def display(img):
-    cv2.line(img,(int(frameWidth/2)-deadZone,0),(int(frameWidth/2)-deadZone,frameHeight),(255,255,0),3)
-    cv2.line(img,(int(frameWidth/2)+deadZone,0),(int(frameWidth/2)+deadZone,frameHeight),(255,255,0),3)
-    cv2.circle(img,(int(frameWidth/2),int(frameHeight/2)),5,(0,0,255),5)
-    cv2.line(img, (0,int(frameHeight / 2) - deadZone), (frameWidth,int(frameHeight / 2) - deadZone),
-            (255, 255, 0), 3)
+    cv2.line(img, (int(frameWidth / 2) - deadZone, 0), (int(frameWidth / 2) - deadZone, frameHeight), (255, 255, 0), 3)
+    cv2.line(img, (int(frameWidth / 2) + deadZone, 0), (int(frameWidth / 2) + deadZone, frameHeight), (255, 255, 0), 3)
+    cv2.circle(img, (int(frameWidth / 2), int(frameHeight / 2)), 5, (0, 0, 255), 5)
+    cv2.line(img, (0, int(frameHeight / 2) - deadZone), (frameWidth, int(frameHeight / 2) - deadZone),
+             (255, 255, 0), 3)
     cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone),
-            (255, 255, 0), 3)
+             (255, 255, 0), 3)
+
 
 # Main loop
 while True:
@@ -156,62 +169,66 @@ while True:
     imgContour = img.copy()
     imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    h_min = cv2.getTrackbarPos("HUE Min","HSV")
+    # Get positions of trackbar at hue, saturation, and value min and max threshold
+    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
     h_max = cv2.getTrackbarPos("HUE Max", "HSV")
     s_min = cv2.getTrackbarPos("SAT Min", "HSV")
     s_max = cv2.getTrackbarPos("SAT Max", "HSV")
     v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
     v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
 
-
-    lower = np.array([h_min,s_min,v_min])
-    upper = np.array([h_max,s_max,v_max])
-    mask = cv2.inRange(imgHsv,lower,upper)
-    result = cv2.bitwise_and(img,img, mask = mask)
+    # Check if HSV values lie between lower and upper hue, saturation, and value bounds. With mask and original image,
+    # obtain new image masked over.
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
+    # Canny edge detection
+    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)  # noise reduction
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
     threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
     threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    # noise reduction, finding intensity gradient, non-maximum suppression, hysteresis thresholding
     imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
     kernel = np.ones((5, 5))
+    # increase area covered by activated pixels, patch up holes between activated pixels
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
     getContours(imgDil, imgContour)
     display(imgContour)
 
     # Initiate flight if drone is not flying
     if startCounter == 0:
-       drone.takeoff()
-       startCounter = 1
+        drone.takeoff()
+        startCounter = 1
 
-
-    # Based on location of object, dir is set, so move accordingly 
+    # Based on location of object, dir is set, so move accordingly
     if dir == 1:
-       drone.yaw_velocity = -60
+        drone.yaw_velocity = -60
     elif dir == 2:
-       drone.yaw_velocity = 60
+        drone.yaw_velocity = 60
     elif dir == 3:
-       drone.up_down_velocity= 60
+        drone.up_down_velocity = 60
     elif dir == 4:
-       drone.up_down_velocity= -60
+        drone.up_down_velocity = -60
     else:
-       drone.left_right_velocity = 0
-       drone.for_back_velocity = 0
-       drone.up_down_velocity = 0
-       drone.yaw_velocity = 0
-    
+        drone.left_right_velocity = 0
+        drone.for_back_velocity = 0
+        drone.up_down_velocity = 0
+        drone.yaw_velocity = 0
+
     # Give velocity info to drone 
     if drone.send_rc_control:
-       drone.send_rc_control(drone.left_right_velocity, 
-                             drone.for_back_velocity, 
-                             drone.up_down_velocity, 
-                             drone.yaw_velocity)
+        drone.send_rc_control(drone.left_right_velocity,
+                              drone.for_back_velocity,
+                              drone.up_down_velocity,
+                              drone.yaw_velocity)
     print(dir)
 
     # Stack images
     stack = stackImages(0.9, ([img, result], [imgDil, imgContour]))
-    cv2.imshow('Horizontal Stacking', stack) # Show images
+    cv2.imshow('Horizontal Stacking', stack)  # Show images
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         drone.land()
